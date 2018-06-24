626aee9eec56d4b4423cab1466958f5c50bc2f0a
keco185 <keco185@me.com>
2018-06-24 09:40:16 -0400

    Midknight Mayhem Code Changes

--- C45513/src/org/usfirst/frc/team484/robot/Robot.java
+++ C45513/src/org/usfirst/frc/team484/robot/Robot.java
@@ -30,9 +30,11 @@ import org.usfirst.frc.team484.robot.commands.auto.AutoDoNothing;
 import org.usfirst.frc.team484.robot.commands.auto.CrossAutoLine;
 import org.usfirst.frc.team484.robot.commands.auto.LeftScaleAndSwitchFromP1;
 import org.usfirst.frc.team484.robot.commands.auto.LeftScaleFromP5;
+import org.usfirst.frc.team484.robot.commands.auto.LeftScaleFrontFromP1;
 import org.usfirst.frc.team484.robot.commands.auto.LeftSwitchFromP3;
 import org.usfirst.frc.team484.robot.commands.auto.RightScaleAndSwitchFromP5;
 import org.usfirst.frc.team484.robot.commands.auto.RightScaleFromP1;
+import org.usfirst.frc.team484.robot.commands.auto.RightScaleFrontFromP5;
 import org.usfirst.frc.team484.robot.commands.auto.RightSwitchFromP3;
 import org.usfirst.frc.team484.robot.commands.auto.SideOfRightSwitchFromP5;
 import org.usfirst.frc.team484.robot.commands.auto.SideOfLeftSwitchFromP1;
@@ -97,14 +99,14 @@ public class Robot extends TimedRobot {
 		GenerateTrajectory.execute("RightScaleFromP1",
 				new Waypoint( 47.3 	,  19.5	, Pathfinder.d2r( 90)),
 				new Waypoint( 47.3	, 167.9	, Pathfinder.d2r( 90)),
-				new Waypoint(121.8	, 233.6 , Pathfinder.d2r(  0)),
-				new Waypoint(220.0	, 233.6	, Pathfinder.d2r(  0)),
-				new Waypoint(279.6	, 285.0	, Pathfinder.d2r( 90)));
+				new Waypoint(121.8	, 240.0 , Pathfinder.d2r(  0)),
+				new Waypoint(220.0	, 240.0	, Pathfinder.d2r(  0)),
+				new Waypoint(255.0	, 285.0	, Pathfinder.d2r( 90)));
 		
 		GenerateTrajectory.execute("LeftScaleFrontFromP1",
 				new Waypoint( 47.3	,  19.5	, Pathfinder.d2r( 90)),
 				new Waypoint( 47.3	, 167.9	, Pathfinder.d2r( 90)),
-				new Waypoint( 101.0	, 280.0	, Pathfinder.d2r( 90)));
+				new Waypoint( 101.0	, 283.0	, Pathfinder.d2r( 90)));
 		
 		GenerateTrajectory.execute("LeftSwitchFromP1",
 				new Waypoint( 47.3 	,  19.5	, Pathfinder.d2r( 90)),
@@ -120,14 +122,14 @@ public class Robot extends TimedRobot {
 		GenerateTrajectory.execute("LeftScaleFromP5",
 				new Waypoint(277.3	,  19.5	, Pathfinder.d2r( 90)),
 				new Waypoint(277.3	, 167.9	, Pathfinder.d2r( 90)),
-				new Waypoint(202.3	, 233.6	, Pathfinder.d2r(180)),
-				new Waypoint(104.0	, 233.6	, Pathfinder.d2r(180)),
-				new Waypoint( 54.4	, 285.0	, Pathfinder.d2r( 90)));
+				new Waypoint(202.3	, 240.0	, Pathfinder.d2r(180)),
+				new Waypoint(104.0	, 240.0	, Pathfinder.d2r(180)),
+				new Waypoint( 54.6+15.0	, 285.0	, Pathfinder.d2r( 90)));
 		
 		GenerateTrajectory.execute("RightScaleFrontFromP5",
 				new Waypoint(277.3	,  19.5	, Pathfinder.d2r( 90)),
 				new Waypoint(277.3	, 167.9	, Pathfinder.d2r( 90)),
-				new Waypoint(226.6	, 280.0	, Pathfinder.d2r( 90)));
+				new Waypoint(226.6	, 283.0	, Pathfinder.d2r( 90)));
 		
 		GenerateTrajectory.execute("RightSwitchFromP5",
 				new Waypoint(277.3	,  19.5	, Pathfinder.d2r( 90)),
@@ -390,6 +392,8 @@ public class Robot extends TimedRobot {
 
 			lrChooser.addObject("Scale", new RightScaleFromP1());
 			rrChooser.addObject("Scale", new RightScaleFromP1());
+			
+			rlChooser.addObject("Scale", new LeftScaleFrontFromP1());
 
 			llChooser.addObject("Scale and Switch", new LeftScaleAndSwitchFromP1());
 			break;
@@ -409,6 +413,8 @@ public class Robot extends TimedRobot {
 
 			llChooser.addObject("Scale", new LeftScaleFromP5());
 			rlChooser.addObject("Scale", new LeftScaleFromP5());
+			
+			lrChooser.addObject("Scale", new RightScaleFrontFromP5());
 			
 			rrChooser.addObject("Scale and Switch", new RightScaleAndSwitchFromP5());
 			break;
